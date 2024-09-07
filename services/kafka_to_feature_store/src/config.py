cee2e91713e667e61fbb4781be6be0648ccd5b75
John Doe <napat.asawa0@gmail.com>
2024-09-07 23:12:14 +0700

    fix configs

--- services/kafka_to_feature_store/src/config.py
+++ services/kafka_to_feature_store/src/config.py
@@ -6,7 +6,5 @@ kafka_consumer_group="trade_to_ohlc_consumer_group"
 feature_group_name="ohlc_feature_group"
 feature_group_version=1
 
-# hopsworks_project_name = os.environ["HOPSWORKS_PROJECT_NAME"]
-# hopsworks_api_key = os.environ["HOPSWORKS_API_KEY"]
-hopsworks_api_key = "bIe6uArciWpewFsy.WxvXDxOwnCsmXeroVYuus5lN3IbLRnrZHsiQlZEn2KXoMn0hr8zfuVuBIMRlJV1n"
-hopsworks_project_name = "nontty12"
+hopsworks_project_name = os.environ["HOPSWORKS_PROJECT_NAME"]
+hopsworks_api_key = os.environ["HOPSWORKS_API_KEY"]
